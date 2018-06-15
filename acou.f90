8ecc78ecbc4f8f45d48b421c586438cb6238a891
funabashi800 <8118524@ed.tus.ac.jp>
2018-06-15 15:29:22 +0900

    add acou.f90

---
+++ acou.f90
@@ -0,0 +1,10 @@
+subroutine acou
+    common /quan2/f(3,3)/quan3/wka(3,3)/den2/den(3) &
+    /const/cons1,cons2,cons3
+    do m = 1,3
+        do n = 1,3
+            wka(m,n)=cons*3*f(m,n)*den(n)
+        enddo
+    enddo
+    return
+end
